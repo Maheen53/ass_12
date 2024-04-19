522974c50ca6c586a2aafeb10a9c892f15508623
“[firstname <Maheen53@users.noreply.github.com>
2024-04-19 15:43:29 -0700

    ass_12

---
+++ main.js
@@ -0,0 +1,16 @@
+// task 12
+let names = ["zayiar", "mahbir", "sanem"];
+for (let i = 0; i < names.length; i++) {
+    console.log(`hi,  ${names[i]} would you like to singing with me`);
+}
+;
+// for each
+names.forEach(list => {
+    console.log(`hi, ${list} today is meeting with you `);
+});
+// 
+for (let newlist of names) {
+    console.log(`hi ${newlist} today i conference with you 2:00pm 
+    regards : by "maheen imtiaz" MI `);
+}
+export {};
